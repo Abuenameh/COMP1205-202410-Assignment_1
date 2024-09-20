5f7e75850bcf0beb6a40c4f51872b7ffd783e82a
Abuenameh <abuenameh@gmail.com>
2024-09-20 13:43:07 -0400

    Update Question4.py

--- Question4.py
+++ Question4.py
@@ -1,4 +1,10 @@
 #Question 4
 """
+The output should be printed using statements with the wording:
 
+print("Point (" + str(x) + ", " + str(y) + ") is in the rectangle")
+
+or
+
+print("Point (" + str(x) + ", " + str(y) + ") is not in the rectangle")
 """
