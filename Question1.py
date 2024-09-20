59d3f19d0ab6581dc31e664f1fc7924e16125757
Abuenameh <abuenameh@gmail.com>
2024-09-20 12:30:34 -0400

    Create Question1.py

---
+++ Question1.py
@@ -0,0 +1,12 @@
+#Question 1
+"""
+Print the results of the calculations in the following format (including the colons):
+
+Plants for each semicircle garden:
+Plants for the circle garden:
+Total plants for garden:
+Soil for each semicircle garden:
+Soil for the circle garden:
+Total soil for the garden:
+Total fill for the garden:
+"""
