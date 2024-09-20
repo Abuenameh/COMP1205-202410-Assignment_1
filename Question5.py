837636afe21f3e514057bd5edeb6aa24ac72f274
Abuenameh <abuenameh@gmail.com>
2024-09-20 13:52:14 -0400

    Create Question5.py

---
+++ Question5.py
@@ -0,0 +1,12 @@
+#Question 5
+"""
+Prompt the teacher for data on a student using code like:
+
+print(f"\nEnter data for student {}:")
+
+where you supply the appropriate contents that go inside {}.
+
+Print the student's grade using code like:
+
+print(f"{name}: {letter_grade}")
+"""
